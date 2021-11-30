3eb9c88441e1be4f599ec1443587f8a3033de82d
pragati1317 <pggupta1317@email.com>
2021-11-30 20:48:12 +0530

    MySpotify

---
+++ script.js
@@ -0,0 +1,67 @@
+console.log("Welcome to spotify");
+//variables of Events
+let songIndex=0;
+let audioElement=new Audio('songs/1.mp3');
+let audioElement1=new Audio('songs/2.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let forward=document.getElementById('forward');
+let gif=document.getElementById('gif');
+let songs=[
+    { songName:"Salam-e-Ishq",filepath:"songs/1.mp3",coverpath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filepath:"songs/2.mp3",coverpath:"covers/2.jpg"},
+    {songName:"Salam-e-Ishq",filepath:"songs/3.mp3",coverpath:"covers/3.jpg"},
+   {songName:"Salam-e-Ishq",filepath:"songs/4.mp3",coverpath:"covers/4.jpg"},
+    {songName:"Salam-e-Ishq",filepath:"songs/5.mp3",coverpath:"covers/5.jpg"},
+    {songName:"Salam-e-Ishq",filepath:"songs/6.mp3",coverpath:"covers/6.jpg"},
+   { songName:"Salam-e-Ishq",filepath:"songs/7.mp3",coverpath:"covers/7.jpg"},
+       { songName:"Salam-e-Ishq",filepath:"songs/8.mp3",coverpath:"covers/8.jpg"},
+       { songName:"Salam-e-Ishq",filepath:"songs/9.mp3",coverpath:"covers/9.jpg"},
+       { songName:"Salam-e-Ishq",filepath:"songs/10.mp3",coverpath:"covers/10.jpg"},
+ //      { songName:"Salam-e-Ishq",filepath:"songs/8.mp3",coverpath:"covers/1.jpg"},
+      
+]
+
+
+ //audioElement.play();
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>
+{
+    if(audioElement.paused || audioElement.currentTime<=0)
+    {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+       gif.style.opacity=1;
+    }
+    else 
+    {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
+    }
+})
+forward.addEventListener('click',()=>
+{
+    if(songIndex<songs.length)
+    {
+         songIndex+=1;
+         this.masterPlay();
+    }
+       
+
+})
+ //Listen to event 
+ audioElement.addEventListener('timeupdate', ()=>
+ {
+      //console.log('timeupdate');
+      //update SeekBar
+      progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+     
+      myProgressBar.value=progress;
+ })
+ myProgressBar.addEventListener('change',()=>
+ {
+     audioElement.currentTime=((myProgressBar.value)/100)*audioElement.duration;
+ })
