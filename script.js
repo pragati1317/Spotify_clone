e30153c68e0408c3f5798a46d333875af723cd69
pragati1317 <pggupta1317@email.com>
2021-12-01 01:01:00 +0530

    upadte1

--- script.js
+++ script.js
@@ -7,20 +7,27 @@ let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
 let forward=document.getElementById('forward');
 let gif=document.getElementById('gif');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
 let songs=[
-    { songName:"Salam-e-Ishq",filepath:"songs/1.mp3",coverpath:"covers/1.jpg"},
-    {songName:"Salam-e-Ishq",filepath:"songs/2.mp3",coverpath:"covers/2.jpg"},
-    {songName:"Salam-e-Ishq",filepath:"songs/3.mp3",coverpath:"covers/3.jpg"},
-   {songName:"Salam-e-Ishq",filepath:"songs/4.mp3",coverpath:"covers/4.jpg"},
-    {songName:"Salam-e-Ishq",filepath:"songs/5.mp3",coverpath:"covers/5.jpg"},
-    {songName:"Salam-e-Ishq",filepath:"songs/6.mp3",coverpath:"covers/6.jpg"},
-   { songName:"Salam-e-Ishq",filepath:"songs/7.mp3",coverpath:"covers/7.jpg"},
-       { songName:"Salam-e-Ishq",filepath:"songs/8.mp3",coverpath:"covers/8.jpg"},
+    { songName:"warriyo -Mortals",filepath:"songs/1.mp3",coverpath:"covers/1.jpg"},
+    {songName:"cielo -Huma -HUma",filepath:"songs/2.mp3",coverpath:"covers/2.jpg"},
+    {songName:"Deaf Kev-Invincible",filepath:"songs/3.mp3",coverpath:"covers/3.jpg"},
+   {songName:"Different Heaven",filepath:"songs/4.mp3",coverpath:"covers/4.jpg"},
+    {songName:"janji -Heroes-Tonight-feat",filepath:"songs/5.mp3",coverpath:"covers/5.jpg"},
+    {songName:"Salam-e-Ishq",filepath:"songs/6.mp3", coverpath:"covers/6.jpg"},
+   { songName:"Salam-e-Ishq",filepath:"songs/7.mp3", coverpath:"covers/7.jpg"},
+       { songName:"Salam-e-Ishq",filepath:"songs/8.mp3", coverpath:"covers/8.jpg"},
        { songName:"Salam-e-Ishq",filepath:"songs/9.mp3",coverpath:"covers/9.jpg"},
        { songName:"Salam-e-Ishq",filepath:"songs/10.mp3",coverpath:"covers/10.jpg"},
  //      { songName:"Salam-e-Ishq",filepath:"songs/8.mp3",coverpath:"covers/1.jpg"},
       
 ]
+songItems.forEach((element,i)=>{ 
+    console.log(element,i);
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+    element.getElementsByTagName("img")[0].src=songs[i].coverpath;
+})
 
 
  //audioElement.play();
